29d84c7b524e5a944a27e9ca813dc1ffa6ffc922
huangjy <huangjy@tuya.com>
2019-07-26 20:36:54 +0800

    CyclicBarrier

---
+++ freedom/src/main/java/com/hjy/learning/freedom/concurrent/cyclicbarrier/CyclicBarrierDemo.java
@@ -0,0 +1,37 @@
+package com.hjy.learning.freedom.concurrent.cyclicbarrier;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by huangjy on 2019/7/26.
+ */
+public class CyclicBarrierDemo {
+    private static CyclicBarrier barrier = new CyclicBarrier(5);
+
+    public static void main(String[] args) throws Exception {
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 10; i++) {
+            final int threadNum = i;
+            Thread.sleep(1000);
+            executor.execute(() -> {
+                try {
+                    race(threadNum);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        executor.shutdown();
+    }
+
+    private static void race(int threadNum) throws Exception {
+        Thread.sleep(1000);
+        System.out.println((threadNum + " is ready"));
+        barrier.await();
+        System.out.println((threadNum + " continue"));
+    }
+}
