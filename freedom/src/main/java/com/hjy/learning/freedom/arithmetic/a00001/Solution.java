98e14eab758dd532b470fddb1ea2bfd763d30591
huangjy <huangjy@tuya.com>
2020-10-23 15:00:58 +0800

    两数之和

---
+++ freedom/src/main/java/com/hjy/learning/freedom/arithmetic/a00001/Solution.java
@@ -0,0 +1,34 @@
+package com.hjy.learning.freedom.arithmetic.a00001;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by huangjy on 2020/10/23.
+ */
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[]{hashtable.get(target - nums[i]), i};
+            }
+            hashtable.put(nums[i], i);
+        }
+        return new int[0];
+    }
+
+    public int[] twoSum1(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[0];
+    }
+
+
+}
